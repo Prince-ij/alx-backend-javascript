9afc7d313fc8ce16364ae511a43b56510aa7ee15
Prince-ij <aminuibrahimthefifth@gmail.com>
2024-06-30 15:50:51 +0100

    0-console.js

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+
+/**
+ * prints a message to the console
+ * @params {string} message - The message to print
+ */
+
+function displayMessage(message) {
+	console.log(message);
+}
+
+
+module.exports = displayMessage;
